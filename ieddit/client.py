f8feb143c16c494f83fb7c7b38d33772a52ace9c
Justin <justin@garofolo.net>
2019-10-23 17:01:22 -0400

    general refactoring of posts (OOP)

--- ieddit/client.py
+++ ieddit/client.py
@@ -1,17 +1,12 @@
-from bs4 import BeautifulSoup
-from urllib.parse import urljoin, quote_plus
 from ._2captcha import _2Captcha
-from datetime import datetime
-import re, requests, sys, os
+import requests
+
+from . import utils
+from .post import Post
 
 class Client:
 
     USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:71.0) Gecko/20100101 Firefox/71.0"
-    IEDDIT = lambda e: urljoin("https://ieddit.com/", e)
-
-    LOG_DIR = os.path.join(os.path.dirname(os.path.abspath(sys.modules['__main__'].__file__)), "__logs__")
-    if not os.path.exists(LOG_DIR):
-        os.mkdir(LOG_DIR)
 
     def __init__(self, username, password, _2captcha_api_key = None, do_login = True):
 
@@ -41,8 +36,8 @@ class Client:
             "password": self.password
         }
 
-        response = self.session.get(Client.IEDDIT("/login/"))
-        response = self.session.post(Client.IEDDIT("/login/"), params)
+        response = self.session.get(utils.IEDDIT("/login/"))
+        response = self.session.post(utils.IEDDIT("/login/"), params)
         cookies = self.session.cookies.get_dict()
 
         if response.status_code != 200:
@@ -55,47 +50,4 @@ class Client:
 
     @_require_login
     def create_post(self, title, sub, url = "", text = "", nsfw = False):
-
-        # send get request and create html parser
-        response = self.session.get(Client.IEDDIT("/create_post"))
-        parser = BeautifulSoup(response.text, "html.parser")
-
-        # determine request parameters
-        params = {
-            "url": url,
-            "self_post_text": text,
-            "title": title,
-            "sub": sub,
-        }
-
-        # if 2captcha key was provided, find base64 captcha data and solve
-        if self._2captcha:
-            img = parser.select_one(".captcha-div img")
-            base64 = img["src"].split()[1]       
-            params["captcha"] = self._2captcha.solve(base64)
-            if not params["captcha"]:
-                raise Exception("failed to solve captcha :(")
-
-        response = self.session.post(Client.IEDDIT("/create_post"), params)
-
-        # retry if the captcha failed
-        if "invalid captcha" in response.text:
-            return self.create_post(title, sub, url, text, nsfw)
-
-        post_url = response.url
-        pattern = r"\/i\/{}\/(\d+)\/".format(sub)
-        match = re.search(pattern, post_url)
-
-        if not match:
-            log_file_name = "create_post " + datetime.now().strftime("%Y-%m-%d %I-%m-%S %p").lower() + ".txt"
-            log_file_path = os.path.join(Client.LOG_DIR, log_file_name)
-            with open(log_file_path, "w") as log_file:
-                log_file.write(response.text)
-            raise Exception("failed to create post: [{}]".format(response.status_code))
-        post_id = int(match.group(1))
-
-        if nsfw:
-            params = { "post_id": post_id }
-            self.session.post(Client.IEDDIT("/user/nsfw"), params)
-            
-        return post_id, post_url
+        return Post.create(self, title, sub, url, text, nsfw)
---
+++ ieddit/utils.py
@@ -0,0 +1,8 @@
+import os, sys
+from urllib.parse import urljoin
+
+IEDDIT = lambda e: urljoin("https://ieddit.com/", e)
+
+LOG_DIR = os.path.join(os.path.dirname(os.path.abspath(sys.modules['__main__'].__file__)), "__logs__")
+if not os.path.exists(LOG_DIR):
+    os.mkdir(LOG_DIR)
